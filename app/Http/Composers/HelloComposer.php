d351a3a215303c58fc66eb9e3473872c6d41e47a
fujino777 <makoto1997312@gmail.com>
2021-08-07 22:14:44 +0900

    チャプター２ルーティングとコントローラ。チャプター３ビューとテンプレート。チャプター４リクエスト・レスポンスを補完する。

---
+++ app/Http/Composers/HelloComposer.php
@@ -0,0 +1,15 @@
+<?php
+namespace App\Http\Composers;
+
+use Illuminate\View\View;
+
+class Hellocomposer
+{
+
+    public function compose(View $view)
+    {
+        $view->with('view_message', 'this view is "' . $view->getName() . '"!!');
+    }
+}
+
+?>
--- test.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use PHPUnit\Framework\Test;
-
-Test
-
-
-?>
